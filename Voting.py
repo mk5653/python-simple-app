244b0070f32e581f77b199d6d54399bb75b5ede2
mk5653 <letmeget437@gmail.com>
2024-08-17 01:50:30 +0100

    added the voting system in the code

---
+++ Voting.py
@@ -0,0 +1,8 @@
+print("Hello to voting system developed by EQL")
+age = int(input("Enter your age: "))
+if age >=18:
+    print ("You are elligible for voting")
+elif age < 18 :
+    print ("you can not vote try next time ")
+
+    
